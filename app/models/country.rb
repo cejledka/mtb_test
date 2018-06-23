6a27a384e379f36fbb235b0b3a658c64db0fddf7
Stanislav Anisimov <s.anisimov23@gmail.com>
2018-06-23 19:34:31 +0300

    Country model validation

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,3 +1,5 @@
 class Country < ApplicationRecord
   has_many :regions
+  validates :currency, presence: true, length: {is: 3}, format: {with: /[A-Z]+/}
+  validates :name, presence: true, uniqueness: true, length: {in: 3..35}
 end
